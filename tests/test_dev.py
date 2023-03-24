2aac303f2dd271655a2d4771399791e1fd96083e
Topaz <topaz1618@163.com>
2023-03-24 18:12:56 +0800

    Test new url

--- tests/test_dev.py
+++ tests/test_dev.py
@@ -4,4 +4,4 @@ import pytest
 def test_dev_api(client):
     response = client.get('/test_dev')
     assert response.status_code == 200
-    assert response.data == b'Deployment success! OMG! '
+    assert response.data == b'Deployment success! '
